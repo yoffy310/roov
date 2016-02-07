e4394700d1e21f77ec94d61abc744559e6de368b
yosuke sato <yoffy@yosuke-sato-no-MacBook-Air.local>
2016-02-07 15:35:32 +0900

    db:migrate after scaffold room

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160207063358) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.text     "room_name",   limit: 65535
+    t.text     "room_genre",  limit: 65535
+    t.text     "room_detail", limit: 65535
+    t.string   "user_id",     limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
