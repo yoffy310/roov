3ccd3b697cdb53a0a16c8f35736f05d4da495ef0
yosuke sato <yoffy@yosuke-sato-no-MacBook-Air.local>
2016-02-07 18:42:30 +0900

    userテーブルにカラム追加設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.for(:sign_up).push(:nickname)
+    devise_parameter_sanitizer.for(:account_update).push(:nickname, :avatar, :age, :sex, :housemate, :occupation)
+  end
+
 end
