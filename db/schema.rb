192062420b0a1b88018551daecbeacd7ce63a16b
yosuke sato <yoffy@yosuke-sato-no-MacBook-Air.local>
2016-02-11 18:46:09 +0900

    age 削除　birth_day追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207103926) do
+ActiveRecord::Schema.define(version: 20160208063604) do
 
   create_table "rooms", force: :cascade do |t|
     t.text     "room_name",   limit: 65535
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20160207103926) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "nickname",               limit: 255
-    t.integer  "age",                    limit: 4
     t.string   "housemate",              limit: 255
     t.string   "occupation",             limit: 255
     t.string   "avatar_file_name",       limit: 255
@@ -48,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160207103926) do
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
     t.string   "sex",                    limit: 255
+    t.date     "birth_day"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
